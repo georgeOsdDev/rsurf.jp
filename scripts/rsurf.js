b240f8a9522b350fdf44d328c7a1ef8c4bd3dde0
Takeharu.Oshida <georgeosddev@gmail.com>
2015-05-04 11:47:07 +0900

    Improve JS

--- scripts/rsurf.js
+++ scripts/rsurf.js
@@ -1,12 +1,16 @@
 (function(global){
 	jQuery(function(){
 		var $arrow     = jQuery("#menuToggleArrow"),
+	      $menuLabel = jQuery("#menu-toggle-arrow"),
 				$slideMenu = jQuery("#secondary");
-		$arrow.on("click", function(e){
+		$menuLabel.on("click", function(e){
 			e.preventDefault();
 			$slideMenu.slideToggle(400, function(){
 				$arrow.toggleClass("flip-vertical");
 			});
 		});
+		setTimeout(function(){
+                        if($menuLabel.css("display") !== "none")  $menuLabel.click();
+		},1000);
 	});
 })(window);
